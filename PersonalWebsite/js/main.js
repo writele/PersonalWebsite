6c3abba01f42c631096362e8ea24ca78a02c5b97
Ele <ecpalmer21@gmail.com>
2016-03-31 14:09:58 -0400

    Small Changes
    Fixed sizing issue with portfolio hover on smaller screen sizes. Fixed
    mobile menu toggle.

--- PersonalWebsite/js/main.js
+++ PersonalWebsite/js/main.js
@@ -2,11 +2,6 @@
 
 	"use strict";	
 
-  
-    $('.navigation').singlePageNav({
-        currentClass : 'active'
-    });
-
 
     $('.toggle-menu').click(function(){
         $('.responsive-menu').stop(true,true).slideToggle();
